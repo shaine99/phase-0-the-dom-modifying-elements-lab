2eb66c0ef076265de5cfc33a1e99ed755dbc1144
Shaine <shaine99waters@gmail.com>
2023-07-03 12:29:03 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var mainNode = document.getElementById("main");
+    if (mainNode) {
+      mainNode.remove();
+    }
+  
+    
+    var newHeader = document.createElement("h1");
+    newHeader.textContent = "YOUR-NAME is the champion";
+    newHeader.id = "victory";
+  
+
+    document.body.appendChild(newHeader);
+  });
+  
